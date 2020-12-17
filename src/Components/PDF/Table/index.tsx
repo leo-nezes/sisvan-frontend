2b88ae5e4fbe68cee81846c791d625cbecd736e0
Leonardo Menezes <leonhard.menezes@gmail.com>
2020-12-17 16:31:42 -0300

    [SISVAN] Criação do relatório - Adicionando imagens ao  relatório - Refatorando criação da tabela

--- src/Components/PDF/Table/index.tsx
+++ src/Components/PDF/Table/index.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import { Text, StyleSheet, View } from '@react-pdf/renderer';
+import { Text, StyleSheet, View, Image } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
-  table: {
+  tableContainer: {
     width: '550',
-    marginVertical: 16,
+    marginVertical: 8,
     borderStyle: 'solid',
     borderWidth: 1,
-    borderRightWidth: 0,
-    borderBottomWidth: 0,
+    // borderRightWidth: 0,
+    // borderBottomWidth: 0,
+  },
+  titleSession: {
+    width: '100%',
+    borderStyle: 'solid',
+    // borderWidth: 1,
+    borderBottom: 1,
+    // borderTopWidth: 0,
+    // borderLeftWidth: 0,
+    // borderRightWidth: 0,
+  },
+  contentSession: {
+    width: '100%',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    backgroundColor: 'tomato',
+  },
+  dataContainer: {
+    width: '50%',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    backgroundColor: 'yellow',
   },
   tableColTitle: {
     width: '100%',
@@ -22,7 +43,7 @@ const styles = StyleSheet.create({
     fontSize: 12,
   },
   tableColSubTitle: {
-    width: '50%',
+    // width: '50%',
     borderStyle: 'solid',
     borderWidth: 1,
     borderLeftWidth: 0,
@@ -39,7 +60,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   tableCol: {
-    width: '25%',
+    width: '50%',
     borderStyle: 'solid',
     borderWidth: 1,
     borderLeftWidth: 0,
@@ -48,6 +69,11 @@ const styles = StyleSheet.create({
   tableCell: {
     marginLeft: 5,
     fontSize: 10,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginVertical: 15,
   },
 });
 
@@ -58,6 +84,7 @@ interface ISubtitleProps {
 
 interface ITableData {
   title: string;
+  img: string;
   subtitleProps: ISubtitleProps[];
 }
 
@@ -66,50 +93,53 @@ interface ITableProps {
 }
 
 const Table = ({ tableData }: ITableProps): any => {
-  return tableData.map((table) => {
+  return tableData.map((table, index) => {
     return (
-      <View style={styles.table}>
+      <View style={styles.tableContainer} break={index === 2}>
         <View style={styles.tableRow}>
-          <View style={styles.tableColTitle}>
+          <View style={styles.titleSession}>
             <Text style={styles.tableCellTitle}>{table.title}</Text>
           </View>
         </View>
 
-        {table.subtitleProps.map((subtitle) => (
-          <>
-            <View>
-              <View style={styles.tableColSubTitle}>
-                <Text style={styles.tableCellSubTitle}>
-                  {subtitle.subtitle}
-                </Text>
-              </View>
-            </View>
-
-            <View style={styles.tableRowData}>
-              <View style={styles.tableRow}>
-                <View style={styles.tableCol}>
-                  <Text style={styles.tableCell}>Quantidade:</Text>
-                </View>
-                <View style={styles.tableCol}>
-                  <Text style={styles.tableCell}>
-                    {subtitle.dataSubtitle[0]}
+        <View style={styles.contentSession}>
+          <View style={styles.dataContainer}>
+            {table.subtitleProps.map((subtitle) => (
+              <>
+                <View style={styles.tableColSubTitle}>
+                  <Text style={styles.tableCellSubTitle}>
+                    {subtitle.subtitle}
                   </Text>
                 </View>
-              </View>
 
-              <View style={styles.tableRow}>
-                <View style={styles.tableCol}>
-                  <Text style={styles.tableCell}>Porcentagem:</Text>
+                <View style={styles.tableRowData}>
+                  <View style={styles.tableRow}>
+                    <View style={styles.tableCol}>
+                      <Text style={styles.tableCell}>Quantidade:</Text>
+                    </View>
+                    <View style={styles.tableCol}>
+                      <Text style={styles.tableCell}>
+                        {subtitle.dataSubtitle[0]}
+                      </Text>
+                    </View>
+                  </View>
+
+                  <View style={styles.tableRow}>
+                    <View style={styles.tableCol}>
+                      <Text style={styles.tableCell}>Porcentagem:</Text>
+                    </View>
+                    <View style={styles.tableCol}>
+                      <Text style={styles.tableCell}>
+                        {subtitle.dataSubtitle[1]}
+                      </Text>
+                    </View>
+                  </View>
                 </View>
-                <View style={styles.tableCol}>
-                  <Text style={styles.tableCell}>
-                    {subtitle.dataSubtitle[1]}
-                  </Text>
-                </View>
-              </View>
-            </View>
-          </>
-        ))}
+              </>
+            ))}
+            <Image style={styles.image} src={table.img} />
+          </View>
+        </View>
       </View>
     );
   });
--- src/Components/PDF/index.tsx
+++ src/Components/PDF/index.tsx
@@ -3,18 +3,54 @@ import { Page, View, Document } from '@react-pdf/renderer';
 
 import Header from './Header';
 import Table from './Table';
+import img from '../../data/pesoXIdade.png';
 
 import { styles } from './styles';
 
 const dataTable = [
   {
     title: 'Peso X Idade',
+    img,
     subtitleProps: [
-      { subtitle: 'Peso muito baixo para a idade', dataSubtitle: [73, 1.4] },
       { subtitle: 'Peso muito baixo para a idade', dataSubtitle: [73, 1.4] },
       { subtitle: 'Peso baixo para a idade', dataSubtitle: [245, 4.71] },
       { subtitle: 'Peso adequado ou eutrófico', dataSubtitle: [4618, 88.77] },
       { subtitle: 'Peso elevado para a idade', dataSubtitle: [266, 5.11] },
+    ],
+  },
+  {
+    title: 'Peso X Altura',
+    img: '../../data/pesoXIdade.png',
+    subtitleProps: [
+      { subtitle: 'Magreza acentuada', dataSubtitle: [136, 2.61] },
+      { subtitle: 'Magreza', dataSubtitle: [166, 3.19] },
+      { subtitle: 'Peso adequado ou eutrófico', dataSubtitle: [3191, 61.34] },
+      { subtitle: 'Risco de sobrepeso', dataSubtitle: [399, 7.67] },
+      { subtitle: 'Obesidade', dataSubtitle: [362, 6.96] },
+    ],
+  },
+  {
+    title: 'Altura X Idade',
+    img: '../../data/pesoXIdade.png',
+    subtitleProps: [
+      {
+        subtitle: 'Altura muito baixa para a idade',
+        dataSubtitle: [487, 9.36],
+      },
+      { subtitle: 'Altura baixa para a idade', dataSubtitle: [717, 13.78] },
+      { subtitle: 'Altura adequada para a idade', dataSubtitle: [3998, 76.86] },
+    ],
+  },
+  {
+    title: 'IMC X Idade',
+    img: '../../data/pesoXIdade.png',
+    subtitleProps: [
+      { subtitle: 'Magreza acentuada', dataSubtitle: [180, 3.48] },
+      { subtitle: 'Magreza', dataSubtitle: [168, 3.23] },
+      { subtitle: 'Eutrofia', dataSubtitle: [2964, 56.96] },
+      { subtitle: 'Risco de sobrepeso', dataSubtitle: [1035, 19.9] },
+      { subtitle: 'Sobrepeso', dataSubtitle: [466, 8.96] },
+      { subtitle: 'Obesidade', dataSubtitle: [389, 7.48] },
     ],
   },
 ];
